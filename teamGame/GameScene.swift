c740a796a6738d596629d99d8f48524d6e867941
Andre <9207andre@gmail.com>
2025-02-13 14:10:30 -0500

    joystick pictures

--- teamGame/GameScene.swift
+++ teamGame/GameScene.swift
@@ -5,11 +5,23 @@
 //  Created by Benjamin Scotti on 2/13/25.
 //
 
+import Foundation
 import SpriteKit
 import GameplayKit
+import GameController
 
 class GameScene: SKScene {
     
-    //Hello this is Andre
+    let joystickContainer = SKSpriteNode(imageNamed: "joystickContainer")
+    let joystickBall = SKSpriteNode(imageNamed: "joystickBall")
+    
+    override func didMove(to view: SKView) {
+        addChild(joystickContainer)
+        addChild(joystickBall)
+        
+        joystickContainer.position = CGPoint(x: frame.midX, y: frame.midY - 500)
+        joystickBall.position = joystickContainer.position
+        
+    }
     
 }
--- teamGame/GameViewController.swift
+++ teamGame/GameViewController.swift
@@ -18,7 +18,7 @@ class GameViewController: UIViewController {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                scene.scaleMode = .aspectFit
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -26,8 +26,8 @@ class GameViewController: UIViewController {
             
             view.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            view.showsFPS = false
+            view.showsNodeCount = false
         }
     }
 
