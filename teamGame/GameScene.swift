e18030e7cf56294377b7387e3e1ff3130d598840
Andre <9207andre@gmail.com>
2025-02-13 14:18:06 -0500

    joystick

--- teamGame/GameScene.swift
+++ teamGame/GameScene.swift
@@ -22,6 +22,28 @@ class GameScene: SKScene {
         joystickContainer.position = CGPoint(x: frame.midX, y: frame.midY - 500)
         joystickBall.position = joystickContainer.position
         
-    }
+    }//end didmove
+     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            joystickBall.position = location
+        }
+    }//end touches began
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            joystickBall.position = location
+        }
+    }//end touches moved
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        joystickBall.position = CGPoint(x: frame.midX, y: frame.midY - 500)
+    }//end touches ended
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }//end update
     
 }
