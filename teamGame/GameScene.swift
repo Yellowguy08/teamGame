e5c82dd8660a45592a015f4f13f930a67e199e02
Yellowguy08 <131368912+Yellowguy08@users.noreply.github.com>
2025-02-13 13:53:23 -0500

    enemy spawn and move to point from off screen

--- teamGame/GameScene.swift
+++ teamGame/GameScene.swift
@@ -10,6 +10,36 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        createEnemy()
+    }
     
+    func createEnemy() {
+        
+        let enemy : SKSpriteNode = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
+        let priority = Int.random(in: 0...1)
+        
+        var x : Int
+        var y : Int
+        
+        if (priority == 1) {
+            x = Int.random(in: 0...Int(frame.width))
+            y = Int.random(in: 0...1) == 1 ? Int(frame.height + enemy.size.height) : 0 - Int(enemy.size.height)
+        } else {
+            x = Int.random(in: 0...1) == 1 ? Int(frame.width + enemy.size.width) : 0 - Int(enemy.size.width)
+            y = Int.random(in: 0...Int(frame.height))
+        }
+        
+        enemy.position = CGPoint(x: x, y: y)
+        
+        addChild(enemy)
+        enemyMove(enemy: enemy)
+        
+    }
+    
+    func enemyMove(enemy : SKSpriteNode) {
+        let move : SKAction = SKAction.move(to: CGPoint(x: frame.width/2, y: frame.height/2), duration: 1)
+        enemy.run(move)
+    }
     
 }
