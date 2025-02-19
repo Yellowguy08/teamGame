13f127a2de95df370c98708c9234bd4faa8aa5c7
GideonDayo <gideon.dayo@csedge.org>
2025-02-19 13:42:19 -0500

    v

--- teamGame/GameScene.swift
+++ teamGame/GameScene.swift
@@ -1,10 +1,3 @@
-//
-//  GameScene.swift
-//  teamGame
-//
-//  Created by Benjamin Scotti on 2/13/25.
-//
-
 import Foundation
 import SpriteKit
 import GameplayKit
@@ -14,7 +7,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let joystickContainer = SKSpriteNode(imageNamed: "joystickContainer")
     let joystickBall = SKSpriteNode(imageNamed: "joystickBall")
-    var player : SKSpriteNode = SKSpriteNode()
+    var player: SKSpriteNode!
+    var movementDirection: CGPoint = .zero
     
     override func didMove(to view: SKView) {
         addChild(joystickContainer)
@@ -22,7 +16,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         joystickContainer.position = CGPoint(x: frame.midX, y: frame.midY - 500)
         joystickBall.position = joystickContainer.position
-        player = childNode(withName: "Player") as! SKSpriteNode
+        
+        if let playerNode = childNode(withName: "Player") as? SKSpriteNode {
+            player = playerNode
+        } else {
+            player = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+            player.position = CGPoint(x: frame.midX, y: frame.midY)
+            player.name = "Player"
+            player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+            player.physicsBody?.affectedByGravity = false
+            player.physicsBody?.isDynamic = true
+            player.physicsBody?.categoryBitMask = 2
+            player.physicsBody?.contactTestBitMask = 4
+            addChild(player)
+        }
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         self.physicsBody = border
@@ -30,54 +37,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.physicsBody?.contactTestBitMask = 2
         
         physicsWorld.contactDelegate = self
-        
-    }//end didmove
-    
+    }
+
     func didBegin(_ contact: SKPhysicsContact) {
-        if (contact.bodyA.categoryBitMask == 4) {
+        if contact.bodyA.categoryBitMask == 4 || contact.bodyB.categoryBitMask == 4 {
             contact.bodyA.node?.removeFromParent()
-        } else {
             contact.bodyB.node?.removeFromParent()
         }
     }
-     
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             joystickBall.position = location
             createEnemy()
         }
-    }//end touches began
+    }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
             joystickBall.position = location
             
-            let v = CGVector(dx: location.x - joystickContainer.position.x, dy: location.y - joystickContainer.position.y)
-//            let angle =
-            
+            let vector = CGVector(dx: location.x - joystickContainer.position.x, dy: location.y - joystickContainer.position.y)
+            movementDirection = CGPoint(x: vector.dx / joystickContainer.size.width, y: vector.dy / joystickContainer.size.height)
         }
-    }//end touches moved
+    }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         joystickBall.position = CGPoint(x: frame.midX, y: frame.midY - 500)
-    }//end touches ended
+    }
     
     override func update(_ currentTime: TimeInterval) {
-        
-    }//end update
-
+        print(movementDirection)
+    }
     
     func createEnemy() {
-        
-        let enemy : SKSpriteNode = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
+        let enemy: SKSpriteNode = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
         let priority = Int.random(in: 0...1)
         
-        var x : Int
-        var y : Int
+        var x: Int
+        var y: Int
         
-        if (priority == 1) {
+        if priority == 1 {
             x = Int.random(in: 0...Int(frame.width))
             y = Int.random(in: 0...1) == 1 ? Int(frame.height + enemy.size.height) : 0 - Int(enemy.size.height)
         } else {
@@ -89,19 +91,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.frame.size)
         enemy.physicsBody?.affectedByGravity = false
         enemy.physicsBody?.isDynamic = false
-        enemy.physicsBody?.pinned = true
         enemy.physicsBody?.categoryBitMask = 4
         enemy.physicsBody?.contactTestBitMask = 2
         
         addChild(enemy)
         enemyMove(enemy: enemy)
-        
     }
     
-    func enemyMove(enemy : SKSpriteNode) {
-        let move : SKAction = SKAction.move(to: CGPoint(x: player.position.x, y: player.position.y), duration: 1)
-        let repeatAction : SKAction = SKAction.repeatForever(move)
+    func enemyMove(enemy: SKSpriteNode) {
+        let move: SKAction = SKAction.move(to: player.position, duration: 1)
+        let repeatAction: SKAction = SKAction.repeatForever(move)
         enemy.run(repeatAction)
     }
-    
 }
