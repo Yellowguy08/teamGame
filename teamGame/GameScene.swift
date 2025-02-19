649cbc326368356cabf807f9179cd16efbd2664d
Andre <9207andre@gmail.com>
2025-02-19 14:08:39 -0500

    Finished joystick (small issue)

--- teamGame/GameScene.swift
+++ teamGame/GameScene.swift
@@ -14,6 +14,7 @@ class GameScene: SKScene {
     
     let joystickContainer = SKSpriteNode(imageNamed: "joystickContainer")
     let joystickBall = SKSpriteNode(imageNamed: "joystickBall")
+    var startedClickInCircle: Bool = false
     
     override func didMove(to view: SKView) {
         addChild(joystickContainer)
@@ -27,24 +28,49 @@ class GameScene: SKScene {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
-            joystickBall.position = location
             createEnemy()
+            
+            if (CGRectContainsPoint(joystickContainer.frame, location)) {
+                startedClickInCircle = true
+                joystickBall.position = location
+            } else {
+                startedClickInCircle = false
+            }
         }
     }//end touches began
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let location = touch.location(in: self)
-            joystickBall.position = location
-            
-            let v = CGVector(dx: location.x - joystickContainer.position.x, dy: location.y - joystickContainer.position.y)
-//            let angle =
-            
-        }
+        updateJoystickBallPosition(touches: touches)
     }//end touches moved
+    
+    func updateJoystickBallPosition(touches: Set<UITouch>) {
+        if startedClickInCircle == true {
+            if let touch = touches.first {
+                let location = touch.location(in: self)
+                
+                let v = CGVector(dx: location.x - joystickContainer.position.x, dy: location.y - joystickContainer.position.y)
+                let angle = atan2(v.dy, v.dx)
+                
+                let deg = angle * CGFloat(180/Double.pi)
+                
+                let length:CGFloat = joystickContainer.frame.height/2
+                
+                let xDistance:CGFloat = sin(angle - 1.57079633) * length
+                let yDistance:CGFloat = cos(angle - 1.57079633) * length
+                
+                if (CGRectContainsPoint(joystickContainer.frame, location)) {
+                    joystickBall.position = location
+                } else {
+                    joystickBall.position = CGPointMake(joystickContainer.position.x - xDistance, joystickContainer.position.y + yDistance)
+                }
+                
+            }
+        }
+    }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         joystickBall.position = CGPoint(x: frame.midX, y: frame.midY - 500)
+        startedClickInCircle = false
     }//end touches ended
     
     override func update(_ currentTime: TimeInterval) {
@@ -73,12 +99,12 @@ class GameScene: SKScene {
         addChild(enemy)
         enemyMove(enemy: enemy)
         
-    }
+    }//end create enemy
     
     func enemyMove(enemy : SKSpriteNode) {
         let move : SKAction = SKAction.move(to: CGPoint(x: Int.random(in: 0...Int(frame.width)), y: Int.random(in: 0...Int(frame.height))), duration: 1)
         let repeatAction : SKAction = SKAction.repeatForever(move)
         enemy.run(repeatAction)
-    }
+    }//end enemy move
     
 }
